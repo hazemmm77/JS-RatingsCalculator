85620f82893d578303df9a17f6ed9c19b1083254
hazemmm777 <hazemmmm7773@gmail.com>
2020-10-13 23:28:07 +0200

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -9,8 +9,8 @@ function collect_ratings() {
 
   elements.forEach(function (element) {
     rating= parseInt(element.id.replace('star',''));
-    ratings.count=parseInt(element.value);
-    ratings.sum=parseInt(element.value)*rating;
+    ratings.count +=parseInt(element.value);
+    ratings.sum +=parseInt(element.value)*rating;
 
    });
    if (ratings.count !=0)
