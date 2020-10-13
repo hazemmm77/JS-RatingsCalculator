a289a121fc31f9c9bd575867788c5b3466cf992e
hazemmm777 <hazemmmm7773@gmail.com>
2020-10-13 23:38:24 +0200

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -5,7 +5,8 @@ function collect_ratings() {
      average:0
    };
    let rating=0;
-   const elements=document.querySelectorAl(' .rating');
+  
+   const elements = document.querySelectorAll('.rating');
 
 
 
