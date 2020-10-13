9c83d748fd0d2b4e24d56256ba0ae0dc989094e0
hazemmm777 <hazemmmm7773@gmail.com>
2020-10-13 18:15:51 +0200

    Update ratings.js

---
+++ js/ratings.js
@@ -0,0 +1,25 @@
+function collect_ratings() {
+   let ratings={
+     count:0,
+     sum:0,
+     average:0
+   };
+   let rating=0;
+   let elements=document.querySelectorAl('.rating');
+
+  elements.forEach(function (element) {
+    rating= parseInt(element.id.replace('star',''));
+    ratings.count=parseInt(element.value);
+    ratings.sum=parseInt(element.value*rating);
+
+   });
+   if (ratings.count !=0)
+      {
+         ratings.average=ratings.sum/ratings.count;
+      }
+   return ratings ;
+}
+document.addEventListener("change", function(){
+const rating= collect_ratings();
+
+});
